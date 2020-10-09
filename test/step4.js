760ce9ecda4f48d19e6d23b0623fd528858d7d52
Matthew Smillie <mbs@tmkf.local>
2020-10-09 16:14:48 -0700

    improve tests on Step 4: mutate()

--- test/step4.js
+++ test/step4.js
@@ -40,6 +40,24 @@ let mutatedDna = baby.mutate();
 let mutated = mutatedDna.some((e, i) => e != originalTestDna[i]);
 assert.equals(TEST_NAME, "mutates something", "", true, mutated);
 
+// assert we still have all the expected DNA letters.
+let dnaSet = new Set(baby.dna);
+assert.equals(
+    TEST_NAME,
+    "doesn't change letters",
+    "",
+    dnaSet.size,
+    originalTestDna.length
+);
+
+// assert that we only changed two
+let totalChanged = baby.dna.reduce((acc, e, i) => {
+    let expect = i.toString().padStart(2, "0");
+    return (acc += expect == e ? 0 : 1);
+}, 0);
+const expectChanged = 2;
+assert.equals(TEST_NAME, "only two changed", "", totalChanged, expectChanged);
+
 // ensure that the original is changed too
 let creatureChanged = baby.dna.some((e, i) => e != originalTestDna[i]);
 assert.equals(
@@ -49,38 +67,5 @@ assert.equals(
     true,
     creatureChanged
 );
+
 assert.endTestBlock();
-
-/* Exercise step four
- * implement mutate()
- *   - should exist on returned object
- *   - when called, should return a new array with two things switched
- *     - can we mock randomIndex to give us two fixed ones? let's try.
- *   - original array should be unchanged -- NEW ARRAY
- */
-
-/*
- * Exercise step five
- * implement compareDNA()
- *   - should exist on new object
- *   - one param
- *   - ADDITION TO SPEC:
- *   -  should _return_ the percentage difference!
- *   -  it's hard to test console logs.
- *   -  math tip: precentage = (differences / 15) * 100
- */
-
-/* Exercise step six
- * implement willLikelySurvive
- *  - should exist
- *  - true when >50% C/G
- *  - false otherwise
- */
-
-/*
- * step seven
- * write a function that returns an array of 30 instances.
- * call it createSample()
- *   - array of length 30
- *   - all specimenNums should be different
- */
